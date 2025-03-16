e9152018613fae577eb84dde08630b2c3156f70b
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-16 19:42:01 +0100

    feat: implement CRUD operations for moods with database integration

--- routes/moods.ts
+++ routes/moods.ts
@@ -5,33 +5,68 @@ import {
 import { zValidator } from '@hono/zod-validator'
 
 import db from '../db'
-import { moods as moodsTable } from '../db/schema/moods'
+import { insertMoodsSchema, moods as moodsTable } from '../db/schema/moods'
 import { desc, eq } from "drizzle-orm";
 import { createPostSchema, fakeMoods } from "../types";
 
 export const moodsRoute = new Hono()
 
-moodsRoute.get("/", context => {
+moodsRoute.get("/", async context => {
 
-    const moods = db.select().from(moodsTable).orderBy(desc(moodsTable.createdAt)).limit(100);
+    const moods = await db.select().from(moodsTable).orderBy(desc(moodsTable.createdAt)).limit(100);
 
     return context.json({ moods: moods });
 })
 
 moodsRoute.post("/", zValidator("json", createPostSchema), async context => {
 
-    const mood = await context.req.valid("json");
-    fakeMoods.push({ ...mood, id: fakeMoods.length + 1 });
+    const mood = context.req.valid("json");
+    const validatedMood = insertMoodsSchema.parse({
+        ...mood,
+    });
+
+    const result = await db
+        .insert(moodsTable)
+        .values(validatedMood)
+        .returning()
+        .then((res) => res[0]);
+
+
     context.status(201);
-    return context.json(mood);
+    return context.json(result);
 })
 
-moodsRoute.get("/:id{[0-9]+}", context => {
+moodsRoute.get("/:id{[0-9]+}", async context => {
     const id = Number.parseInt(context.req.param("id"));
-    const mood = fakeMoods.find(mood => mood.id === id);
+    const mood = await db
+        .select()
+        .from(moodsTable)
+        .where(eq(moodsTable.id, id))
+        .then((res) => res[0]);
+
     if (!mood) {
         return context.notFound();
     }
+
     return context.json({ mood });
 }
 )
+
+moodsRoute.delete("/:id{[0-9]+}", async context => {
+    const id = Number.parseInt(context.req.param("id"));
+    //const user = context.var.user;
+
+    const mood = await db
+        .delete(moodsTable)
+        .where(eq(moodsTable.id, id))
+        .returning()
+        .then((res) => res[0]);
+
+    if (!mood) {
+        return context.notFound();
+    }
+
+    return context.json({ mood: mood });
+}
+)
+
