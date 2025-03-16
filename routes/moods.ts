8d0d04b0d38d692ace97cca079c42eee9a2e3fb6
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-16 18:58:07 +0100

    feat: implement moods route with database integration and remove fake data

--- routes/moods.ts
+++ routes/moods.ts
@@ -1,72 +1,37 @@
-import { Hono
+import {
+    Hono
 } from "hono";
 
-import { z } from "zod";
 import { zValidator } from '@hono/zod-validator'
 
+import db from '../db'
+import { moods as moodsTable } from '../db/schema/moods'
+import { desc, eq } from "drizzle-orm";
+import { createPostSchema, fakeMoods } from "../types";
 
-const moodSchema = z.object({   
-    id: z.number().int().positive().min(1),
-    level: z.string(),
-    emoji: z.string().emoji(),
-    date: z.string(),
-    notes: z.string().optional()
-});
-
-type Mood = z.infer<typeof moodSchema>;
-
-const createPostSchema = moodSchema.omit({id: true});
-
-const fakeMoods: Mood[] = [
-    {
-        id: 1,
-        level: "happy",
-        emoji: "😊",
-        date: "2023-10-01",
-        notes: "Had a great day at the park."
-    },
-    {
-        id: 2,
-        level: "sad",
-        emoji: "😢",
-        date: "2023-10-02",
-        notes: "Lost my wallet."
-    },
-    {
-        id: 3,
-        level: "excited",
-        emoji: "😃",
-        date: "2023-10-03",
-        notes: "Got a new job!"
-    },
-    {
-        id: 4,
-        level: "angry",
-        emoji: "😡",
-        date: "2023-10-04",
-        notes: "Stuck in traffic for hours."
-    }
-]
-
-
- 
 export const moodsRoute = new Hono()
-    .get("/", context => {
-        return context.json({moods: fakeMoods});
-    })
-    .post("/", zValidator("json", createPostSchema), async context => {
 
-        const mood = await context.req.valid("json");
-        fakeMoods.push({...mood, id: fakeMoods.length + 1});
-        context.status(201);
-        return context.json(mood);
-    })
-    .get("/:id{[0-9]+}", context => {
-        const id = Number.parseInt(context.req.param("id"));
-        const mood = fakeMoods.find(mood => mood.id === id);
-        if (!mood) {
-            return context.notFound();
-        }
-        return context.json({mood});
-    }   
-    )
+moodsRoute.get("/", context => {
+
+    const moods = db.select().from(moodsTable).orderBy(desc(moodsTable.createdAt)).limit(100);
+
+    return context.json({ moods: moods });
+})
+
+moodsRoute.post("/", zValidator("json", createPostSchema), async context => {
+
+    const mood = await context.req.valid("json");
+    fakeMoods.push({ ...mood, id: fakeMoods.length + 1 });
+    context.status(201);
+    return context.json(mood);
+})
+
+moodsRoute.get("/:id{[0-9]+}", context => {
+    const id = Number.parseInt(context.req.param("id"));
+    const mood = fakeMoods.find(mood => mood.id === id);
+    if (!mood) {
+        return context.notFound();
+    }
+    return context.json({ mood });
+}
+)
