369c42c5f05954c3a18d10e15b396f5133d2b6d4
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-26 15:28:35 +0100

    refactor: improve formatting and consistency in moods.ts

--- routes/moods.ts
+++ routes/moods.ts
@@ -1,72 +1,65 @@
+import { zValidator } from "@hono/zod-validator";
+import { desc, eq } from "drizzle-orm";
 import {
-    Hono
+  Hono,
 } from "hono";
 
-import { zValidator } from '@hono/zod-validator'
-
-import db from '../db'
-import { insertMoodsSchema, moods as moodsTable } from '../db/schema/moods'
-import { desc, eq } from "drizzle-orm";
+import db from "../db";
+import { insertMoodsSchema, moods as moodsTable } from "../db/schema/moods";
 import { createPostSchema } from "../types";
 
-export const moodsRoute = new Hono()
+export const moodsRoute = new Hono();
 
-moodsRoute.get("/", async context => {
+moodsRoute.get("/", async (context) => {
+  const moods = await db.select().from(moodsTable).orderBy(desc(moodsTable.createdAt)).limit(100);
 
-    const moods = await db.select().from(moodsTable).orderBy(desc(moodsTable.createdAt)).limit(100);
+  return context.json({ moods });
+});
 
-    return context.json({ moods: moods });
-})
+moodsRoute.post("/", zValidator("json", createPostSchema), async (context) => {
+  const mood = context.req.valid("json");
+  const validatedMood = insertMoodsSchema.parse({
+    ...mood,
+  });
 
-moodsRoute.post("/", zValidator("json", createPostSchema), async context => {
+  const result = await db
+    .insert(moodsTable)
+    .values(validatedMood)
+    .returning()
+    .then(res => res[0]);
 
-    const mood = context.req.valid("json");
-    const validatedMood = insertMoodsSchema.parse({
-        ...mood,
-    });
+  context.status(201);
+  return context.json(result);
+});
 
-    const result = await db
-        .insert(moodsTable)
-        .values(validatedMood)
-        .returning()
-        .then((res) => res[0]);
+moodsRoute.get("/:id{[0-9]+}", async (context) => {
+  const id = Number.parseInt(context.req.param("id"));
+  const mood = await db
+    .select()
+    .from(moodsTable)
+    .where(eq(moodsTable.id, id))
+    .then(res => res[0]);
 
+  if (!mood) {
+    return context.notFound();
+  }
 
-    context.status(201);
-    return context.json(result);
-})
+  return context.json({ mood });
+});
 
-moodsRoute.get("/:id{[0-9]+}", async context => {
-    const id = Number.parseInt(context.req.param("id"));
-    const mood = await db
-        .select()
-        .from(moodsTable)
-        .where(eq(moodsTable.id, id))
-        .then((res) => res[0]);
+moodsRoute.delete("/:id{[0-9]+}", async (context) => {
+  const id = Number.parseInt(context.req.param("id"));
+  // const user = context.var.user;
 
-    if (!mood) {
-        return context.notFound();
-    }
+  const mood = await db
+    .delete(moodsTable)
+    .where(eq(moodsTable.id, id))
+    .returning()
+    .then(res => res[0]);
 
-    return context.json({ mood });
-}
-)
+  if (!mood) {
+    return context.notFound();
+  }
 
-moodsRoute.delete("/:id{[0-9]+}", async context => {
-    const id = Number.parseInt(context.req.param("id"));
-    //const user = context.var.user;
-
-    const mood = await db
-        .delete(moodsTable)
-        .where(eq(moodsTable.id, id))
-        .returning()
-        .then((res) => res[0]);
-
-    if (!mood) {
-        return context.notFound();
-    }
-
-    return context.json({ mood: mood });
-}
-)
-
+  return context.json({ mood });
+});
