e30092379294197a02d82c2176b6aae3e5408527
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-26 15:25:46 +0100

    refactor: improve import statements and formatting in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig, type Config } from 'drizzle-kit';
+import type { Config } from "drizzle-kit";
+
+import { defineConfig } from "drizzle-kit";
+import process from "node:process";
 
 export default defineConfig({
-    out: './drizzle',
-    schema: './db/schema/*',
-    dialect: 'sqlite',
-    dbCredentials: {
-        url: process.env.DATABASE_URL as string,
-    },
+  out: "./drizzle",
+  schema: "./db/schema/*",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+
+  },
 }) satisfies Config;
