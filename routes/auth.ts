379d1095d1df36fdbc315f77769a144ae9f5cd6c
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-26 15:28:22 +0100

    refactor: improve formatting and consistency in auth.ts

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,36 +1,34 @@
-
 import {
-    Hono
+  Hono,
 } from "hono";
 
-import { kindeClient, sessionManager, getUser } from '../kinde'
+import { getUser, kindeClient, sessionManager } from "../kinde";
 
-
-export const authRoute = new Hono()
+export const authRoute = new Hono();
 
 authRoute.get("/login", async (context) => {
-    const loginUrl = await kindeClient.login(sessionManager(context));
-    return context.redirect(loginUrl.toString());
+  const loginUrl = await kindeClient.login(sessionManager(context));
+  return context.redirect(loginUrl.toString());
 });
 
 authRoute.get("/register", async (context) => {
-    const registerUrl = await kindeClient.register(sessionManager(context));
-    return context.redirect(registerUrl.toString());
+  const registerUrl = await kindeClient.register(sessionManager(context));
+  return context.redirect(registerUrl.toString());
 });
 
 authRoute.get("/callback", async (context) => {
-    //get called every time the user login or register
-    const url = new URL(context.req.url);
-    await kindeClient.handleRedirectToApp(sessionManager(context), url);
-    return context.redirect("/");
+  // get called every time the user login or register
+  const url = new URL(context.req.url);
+  await kindeClient.handleRedirectToApp(sessionManager(context), url);
+  return context.redirect("/");
 });
 
 authRoute.get("/logout", async (context) => {
-    const logoutUrl = await kindeClient.logout(sessionManager(context));
-    return context.redirect(logoutUrl.toString());
+  const logoutUrl = await kindeClient.logout(sessionManager(context));
+  return context.redirect(logoutUrl.toString());
 });
 
 authRoute.get("/me", getUser, async (context) => {
-   const user = context.var.user;
-    return context.json({ user });
+  const user = context.var.user;
+  return context.json({ user });
 });
