4ed27a00e1c109719e8bf92240d69d4fa8f544e3
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-30 00:52:54 +0100

    feat: add moods schema with insert and select validation

---
+++ backend/db/schema/moods.ts
@@ -0,0 +1,20 @@
+import { sql } from "drizzle-orm";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+export const moods = sqliteTable("moods_table", {
+  id: int().primaryKey({ autoIncrement: true }),
+  userID: text("user_id").notNull(),
+  type: text().notNull(),
+  emoji: text().notNull(),
+  note: text(),
+  createdAt: text("created_at").notNull().default(sql`(current_timestamp)`),
+});
+
+// Schema for inserting a user - can be used to validate API requests
+export const insertMoodsSchema = createInsertSchema(moods, {
+  emoji: z.string().emoji(),
+});
+// Schema for selecting a Mood - can be used to validate API responses
+export const selectMoodsSchema = createSelectSchema(moods);
