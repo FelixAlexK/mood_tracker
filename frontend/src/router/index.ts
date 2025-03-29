ba0b163354e9bf821e9987351e86806647c0c116
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-30 00:54:55 +0100

    feat: add Vue Router setup with initial routes for home and about views

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,15 @@
+import type { RouteRecordRaw } from "vue-router";
+
+import { createRouter, createWebHistory } from "vue-router";
+
+export const routes: RouteRecordRaw[] = [
+  { path: "/", component: () => import("../views/home-view.vue"), name: "Home" },
+  { path: "/about", component: () => import("../views/about-view.vue"), name: "About" },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
