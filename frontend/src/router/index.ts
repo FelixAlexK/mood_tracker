05d1b00a5f28f00bfa7ce53dc3743b274bb2f07f
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-30 23:04:26 +0200

    feat: rename home route to "Mood Tracker" for clarity

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,7 +3,7 @@ import type { RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHistory } from "vue-router";
 
 export const routes: RouteRecordRaw[] = [
-  { path: "/", component: () => import("../views/home-view.vue"), name: "Home" },
+  { path: "/", component: () => import("../views/home-view.vue"), name: "Mood Tracker" },
   { path: "/about", component: () => import("../views/about-view.vue"), name: "About" },
   { path: "/create-mood", component: () => import("../views/create-view.vue"), name: "Create" },
 ];
