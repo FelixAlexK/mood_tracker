babf824c985507ab5db48bb5825a62f023e280b6
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-03-30 22:33:35 +0200

    feat: integrate MoodForm and MoodCard components into home view for mood entry display and submission

--- frontend/src/views/home-view.vue
+++ frontend/src/views/home-view.vue
@@ -1,51 +1,43 @@
 <script setup lang="ts">
-import { api } from "@/lib/api";
-import { useQuery } from "@tanstack/vue-query";
+import MoodCardComponent from "@/components/mood-card-component.vue";
+import MoodFormComponent from "@/components/mood-form-component.vue";
+import { getMoods } from "@/lib/api";
+import { useMutationState, useQuery } from "@tanstack/vue-query";
 
-async function getMoods() {
-  const result = await api.moods.$get();
-  if (!result.ok) {
-    throw new Error("Failed to fetch moods");
-  }
-  const data = await result.json();
-  return data;
-}
+import type { MoodEntry } from "../types";
 
-const { isPending, isError, data, error } = useQuery({
+const { data } = useQuery({
   queryKey: ["get-moods"],
   queryFn: getMoods,
 });
 
-function formattedDate(createdAt: string | null) {
-  return createdAt
-    ? new Date(createdAt).toLocaleDateString("en-US", {
-        month: "short",
-        day: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-      })
-    : "No date";
-}
+const variables = useMutationState<MoodEntry>({
+  filters: { mutationKey: ["create-mood"], status: "pending" },
+  select: mutation => mutation.state.variables as MoodEntry,
+});
 </script>
 
 <template>
-  <span v-if="isPending">Loading...</span>
-  <span v-else-if="isError">Error: {{ error?.message }}</span>
-  <div v-else class=" grid grid-cols-1 md:grid-cols-2 gap-4">
-    <div v-for="mood in data?.moods" :key="mood.id" class="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow ">
-      <div class="flex items-center justify-between mb-2">
-        <span class="text-2xl">{{ mood.emoji }}</span>
-        <span class="text-sm text-gray-600 capitalize">{{ mood.type }}</span>
-      </div>
+  <div>
+    <section>
+      <MoodFormComponent />
+    </section>
+    <section>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <MoodCardComponent
+          v-for="mood in data?.moods"
+          :key="mood.id"
+          :mood="mood"
+          :variables="variables"
+        />
 
-      <p v-if="mood.note" class="text-gray-700 mb-3 line-clamp-2">
-        {{ mood.note }}
-      </p>
-
-      <div class="flex items-center text-gray-500 text-sm">
-        <!-- <Clock class-name="w-4 h-4 mr-1" /> -->
-        <span>{{ formattedDate(mood.createdAt) }}</span>
+        <MoodCardComponent
+          v-for="mood in variables"
+          v-show="variables"
+          :key="mood.id"
+          :mood="mood"
+        />
       </div>
-    </div>
+    </section>
   </div>
 </template>
