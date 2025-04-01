001dc3722fed252ab3c28197a19e4ae9b2fff5f9
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-02 01:39:22 +0200

    feat: add headline component with props for dynamic text rendering

---
+++ frontend/src/components/headline-component.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { text } = defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+  <section class="pt-24 pb-12 mx-auto">
+    <div class="mx-auto">
+      <div>
+        <h2 class="text-dark mb-2 text-2xl font-semibold ">
+          {{ text }}
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
