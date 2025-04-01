e53b80e0b5e924eb64b2d529f8ca15f213dbed30
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-02 01:39:11 +0200

    feat: remove unnecessary section header from app layout

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -7,15 +7,6 @@ import NavComponent from "./components/nav-component.vue";
     <NavComponent />
     <main class="min-h-screen bg-gray-100 p-6">
       <div class="max-w-2xl mx-auto">
-        <section class="pt-24 pb-12 mx-auto">
-          <div class="mx-auto">
-            <div>
-              <h2 class="text-dark mb-2 text-2xl font-semibold ">
-                {{ $route.name }}
-              </h2>
-            </div>
-          </div>
-        </section>
         <RouterView />
       </div>
     </main>
