895c2688d5380af03bd5647687f1efc18ca6a1a9
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-05 00:52:09 +0200

    fix: update route path for my moods and ensure scroll behavior is set to top

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 export const routes: RouteRecordRaw[] = [
   { path: "/", component: () => import("../views/home-view.vue"), name: "Mood Tracker" },
-  { path: "/my-moods", component: () => import("../views/my-moods.vue"), name: "My Moods" },
+  { path: "/moods", component: () => import("../views/my-moods.vue"), name: "" },
   { path: "/mood/:id", component: () => import("../views/mood-detail.vue"), name: "", props: true },
   { path: "/stats", component: () => import("../views/stats-view.vue"), name: "Stats" },
   { path: "/about", component: () => import("../views/about-view.vue"), name: "About" },
@@ -13,6 +13,10 @@ export const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    // always scroll to top
+    return { top: 0, behavior: "instant" };
+  },
 });
 
 export default router;
