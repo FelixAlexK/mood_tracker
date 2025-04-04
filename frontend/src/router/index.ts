ef8bf6e362af94fd4cc740ed4db43d5d87d4e266
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-04 16:07:16 +0200

    fix: reorder routes to ensure consistent path definitions in router configuration

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,9 +5,9 @@ import { createRouter, createWebHistory } from "vue-router";
 export const routes: RouteRecordRaw[] = [
   { path: "/", component: () => import("../views/home-view.vue"), name: "Mood Tracker" },
   { path: "/my-moods", component: () => import("../views/my-moods.vue"), name: "My Moods" },
-  { path: "/about", component: () => import("../views/about-view.vue"), name: "About" },
   { path: "/mood/:id", component: () => import("../views/mood-detail.vue"), name: "", props: true },
   { path: "/stats", component: () => import("../views/stats-view.vue"), name: "Stats" },
+  { path: "/about", component: () => import("../views/about-view.vue"), name: "About" },
 ];
 
 const router = createRouter({
