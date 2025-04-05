48d5fc18da17301d6c3f5eb259dafbaf9432e365
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-05 12:55:25 +0200

    feat: implement user authentication check in router before navigation

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,10 @@
 import type { RouteRecordRaw } from "vue-router";
 
+import { userQueryOptions } from "@/lib/api";
+import { QueryClient } from "@tanstack/vue-query";
 import { createRouter, createWebHistory } from "vue-router";
+
+const queryClient = new QueryClient();
 
 export const routes: RouteRecordRaw[] = [
   { path: "/", component: () => import("../views/home-view.vue"), name: "Mood Tracker" },
@@ -8,6 +12,7 @@ export const routes: RouteRecordRaw[] = [
   { path: "/mood/:id", component: () => import("../views/mood-detail.vue"), name: "", props: true },
   { path: "/stats", component: () => import("../views/stats-view.vue"), name: "Stats" },
   { path: "/about", component: () => import("../views/about-view.vue"), name: "About" },
+  { path: "/profile", component: () => import("../views/profile.view.vue"), name: "Profile" },
 ];
 
 const router = createRouter({
@@ -17,6 +22,26 @@ const router = createRouter({
     // always scroll to top
     return { top: 0, behavior: "instant" };
   },
+});
+
+router.beforeEach(async (to, _from) => {
+  let user = queryClient.getQueryData(userQueryOptions.queryKey);
+
+  try {
+    user = await queryClient.fetchQuery(
+      userQueryOptions,
+    );
+  }
+  catch {
+    user = undefined;
+  }
+
+  if (
+    !user && to.name !== "Login"
+  ) {
+    // redirect the user to the login page
+    return { name: "Login" };
+  }
 });
 
 export default router;
