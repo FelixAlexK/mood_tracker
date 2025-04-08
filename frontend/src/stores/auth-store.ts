9a201ab2a52f9678c98786dbdce87fef6daf305b
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-09 01:11:43 +0200

    refactor: add user state management with setUser and getUser methods in auth store

--- frontend/src/stores/auth-store.ts
+++ frontend/src/stores/auth-store.ts
@@ -1,8 +1,11 @@
+import type { UserType } from "@/types";
+
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useAuthStore = defineStore("auth", () => {
   const isLoggedIn = ref(false);
+  const user = ref<UserType | undefined>(undefined);
 
   // Action to set the logged-in state
   const setLoggedIn = (status: boolean) => {
@@ -14,5 +17,13 @@ export const useAuthStore = defineStore("auth", () => {
     return isLoggedIn.value;
   };
 
-  return { isLoggedIn, setLoggedIn, getLoggedIn };
+  const setUser = (userData: UserType | undefined) => {
+    user.value = userData;
+  };
+
+  const getUser = () => {
+    return user.value;
+  };
+
+  return { isLoggedIn, setLoggedIn, getLoggedIn, setUser, getUser, user };
 });
