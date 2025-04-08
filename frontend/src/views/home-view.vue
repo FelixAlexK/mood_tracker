8ec58e748e2b8657ff0b00d0b73834534c6b6505
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-09 01:12:01 +0200

    refactor: update home-view layout and improve mood entry display

--- frontend/src/views/home-view.vue
+++ frontend/src/views/home-view.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import ButtonComponent from "@/components/button-component.vue";
+import HeadlineComponent from "@/components/headline-component.vue";
 import MoodCardComponent from "@/components/mood-card-component.vue";
 import MoodFormComponent from "@/components/mood-form-component.vue";
 import { getMoods } from "@/lib/api";
 import router from "@/router";
 import { useAuthStore } from "@/stores/auth-store";
 import { useMutationState, useQuery } from "@tanstack/vue-query";
+import { ArrowLeft } from "lucide-vue-next";
 
 import type { MoodEntry } from "../types";
 
 const authStore = useAuthStore();
 
-const PAGE_SIZE = 6;
+const PAGE_SIZE = 1;
 const PAGE = 1;
 
 const { data } = useQuery({
@@ -27,30 +29,31 @@ const variables = useMutationState<MoodEntry>({
 </script>
 
 <template>
-  <div class="mt-16">
-    <section>
-      <MoodFormComponent />
-    </section>
-    <section v-if="authStore.isLoggedIn">
+  <div class="max-w-3xl mx-auto">
+    <HeadlineComponent
+      :text="`Welcome ${authStore.user?.given_name ?? ''}!`"
+    />
+
+    <MoodFormComponent />
+
+    <div v-if="authStore.isLoggedIn">
       <h3 class="block text-gray-700 text-sm font-bold mb-2 pt-4">
-        Latest Moods
+        Latest Entry
       </h3>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div class="">
         <MoodCardComponent
           v-for="mood in data?.moods"
           :key="mood.id"
           :mood="mood"
-          :variables="variables"
         />
 
         <MoodCardComponent
           v-for="mood in variables"
-          v-show="variables"
           :key="mood.id"
           :mood="mood"
         />
       </div>
       <ButtonComponent :disabled="(data?.total ?? 0) <= 6" class="mt-4" text="See All" @click="router.push({ path: '/moods' })" />
-    </section>
+    </div>
   </div>
 </template>
