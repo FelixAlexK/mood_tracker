444ab353a7b36605af3ef70eb283a524c99eddde
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-09 01:11:29 +0200

    refactor: improve mood-card-component structure and enhance click handling

--- frontend/src/components/mood-card-component.vue
+++ frontend/src/components/mood-card-component.vue
@@ -8,20 +8,26 @@ import { Clock } from "lucide-vue-next";
 const { mood } = defineProps<{
   mood: MoodEntry;
 }>();
+
+function handleRouterPush() {
+  router.push(`/mood/${mood.id}`);
+}
 </script>
 
 <template>
-  <div class="relative bg-white rounded-lg shadow-md w-full h-32 p-4 hover:shadow-lg transition-shadow" @click="router.push(`/mood/${mood.id}`)">
-    <div class="flex items-center justify-between mb-2">
-      <span class="text-2xl">{{ mood.emoji }}</span>
-      <span class="text-sm text-gray-600 capitalize">{{ mood.type }}</span>
+  <div class="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow cursor-pointer flex flex-col h-full" @click="handleRouterPush">
+    <div class="flex items-center justify-between mb-2 ">
+      <span class="text-2xl block">{{ mood.emoji }}</span>
+      <span class="text-sm capitalize">{{ mood.type }}</span>
     </div>
-    <p v-if="mood.note" class="text-gray-700 mb-3 line-clamp-2">
-      {{ mood.note }}
+
+    <p class="text-gray-700 my-2 line-clamp-2 ">
+      {{ mood.note ?? '-' }}
     </p>
-    <div class="absolute bottom-2 left-4 flex items-center text-gray-500 text-sm ">
-      <Clock class="w-4 h-4 mr-1" />
-      <span>{{ formattedDate(mood.createdAt) }}</span>
+
+    <div class="flex items-center text-gray-500 text-sm mt-auto ">
+      <Clock class="w-4 h-4 mr-1 " />
+      <time :datetime="mood.createdAt ?? ''">{{ formattedDate(mood.createdAt) }}</time>
     </div>
   </div>
 </template>
