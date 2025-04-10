70710b05ae5d93d1337e265f00422139011887fe
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-04-10 16:08:05 +0200

    refactor: enhance button component with primary prop and update styling for better visual distinction

--- frontend/src/components/button-component.vue
+++ frontend/src/components/button-component.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-const { type = "button", disabled = false } = defineProps<{
+const { type = "button", disabled = false, primary = false } = defineProps<{
   type?: "button" | "submit" | "reset" | undefined;
   text: string;
   disabled?: boolean;
+  primary?: boolean;
 }>();
 
 defineEmits(["click"]);
@@ -11,9 +12,14 @@ defineEmits(["click"]);
 <template>
   <button
     :type="type"
-    class="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition-colors flex items-center justify-center gap-2"
+    class="w-full  text-lg text-gray-100 py-2 font-bold px-4 rounded-lg transition-colors flex items-center justify-center gap-2 "
     :class="{
-      'opacity-50 cursor-not-allowed': disabled,
+
+      'bg-gray-600 hover:bg-gray-500/70': primary,
+      'bg-gray-400 hover:bg-gray-400/70': !primary,
+      'cursor-not-allowed opacity-50': disabled,
+      'cursor-pointer': !disabled,
+
     }"
     @click="!disabled && $emit('click')"
   >
