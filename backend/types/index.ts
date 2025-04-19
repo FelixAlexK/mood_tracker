f7238b9e16e513b93218ec1949f96a7e23766ba4
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-04-19 06:25:55 +0200

    feat: add streak schema and validation for improved data handling

--- backend/types/index.ts
+++ backend/types/index.ts
@@ -9,7 +9,21 @@ export const moodSchema = z.object({
   user_id: z.string(),
 });
 
-export type Mood = z.infer<typeof moodSchema>;
+export const streakSchema = z.object({
+  id: z.number().int().positive().min(1),
+  created_at: z.optional(z.number()), // Store UNIX timestamp in milliseconds
+  updated_at: z.optional(z.date()), // Store UNIX timestamp in milliseconds
+  streak_count: z.number().int().positive(),
+  longest_streak: z.number().int().positive(),
+  streak_status: z.boolean(),
+  user_id: z.string(),
+});
 
-export const createPostSchema = moodSchema.omit({ id: true, user_id: true, created_at: true });
-export const updatePostSchema = moodSchema.partial().omit({ id: true, created_at: true, user_id: true });
+export type Mood = z.infer<typeof moodSchema>;
+export type Streak = z.infer<typeof streakSchema>;
+
+export const moodPostValidationSchema = moodSchema.omit({ id: true, user_id: true, created_at: true });
+export const streakPostValidationSchema = streakSchema.omit({ id: true, user_id: true, created_at: true });
+
+export const moodPostUpdateSchema = moodSchema.partial().omit({ id: true, created_at: true, user_id: true });
+export const streakPostUpdateSchema = streakSchema.partial().omit({ id: true, created_at: true, user_id: true });
