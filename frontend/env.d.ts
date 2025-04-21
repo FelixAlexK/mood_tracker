896e0638a581f06b79ea464e6930b9c5fc447541
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-04-21 23:28:15 +0200

    fix: add type definitions for bun environment variables

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare module "bun" {
+    type Env = {
+        DATABASE_URL: string;
+        KINDE_ISSUER_URL: string;
+        KINDE_CLIENT_ID: string;
+        KINDE_CLIENT_SECRET: string;
+        KINDE_SITE_URL: string;
+        KINDE_LOGOUT_REDIRECT_URI: string;
+        KINDE_REDIRECT_URI: string;
+        KINDE_DOMAIN: string;
+
+    };
+}
