26f08d980941786698fd7c153cc066ac4e0a8a48
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-04-23 21:13:21 +0200

    fix: standardize error handling formatting and improve readability in app.ts; update env.d.ts for consistent indentation

--- backend/app.ts
+++ backend/app.ts
@@ -14,15 +14,15 @@ const app = new Hono();
 app.use("*", logger());
 
 export function errorHandler(err: Error | HTTPException, c: Context) {
-    console.log("=== Caught Error ===");
-    if (err instanceof HTTPException) {
-        return c.text(err.message, err.status);
-    }
-    if (err instanceof z.ZodError) {
-        return c.text(err.errors.map(err => err.message).join(",\n"), 400);
-    }
-    console.error(err);
-    return c.text("Something went wrong", 500);
+  console.log("=== Caught Error ===");
+  if (err instanceof HTTPException) {
+    return c.text(err.message, err.status);
+  }
+  if (err instanceof z.ZodError) {
+    return c.text(err.errors.map(err => err.message).join(",\n"), 400);
+  }
+  console.error(err);
+  return c.text("Something went wrong", 500);
 }
 
 const _apiRoutes = app.basePath("/api").route("/moods", moodsRoute).route("/", authRoute).route("/stats", statsRoute);
--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -2,14 +2,14 @@
 
 declare module "bun" {
     type Env = {
-        DATABASE_URL: string;
-        KINDE_ISSUER_URL: string;
-        KINDE_CLIENT_ID: string;
-        KINDE_CLIENT_SECRET: string;
-        KINDE_SITE_URL: string;
-        KINDE_LOGOUT_REDIRECT_URI: string;
-        KINDE_REDIRECT_URI: string;
-        KINDE_DOMAIN: string;
+      DATABASE_URL: string;
+      KINDE_ISSUER_URL: string;
+      KINDE_CLIENT_ID: string;
+      KINDE_CLIENT_SECRET: string;
+      KINDE_SITE_URL: string;
+      KINDE_LOGOUT_REDIRECT_URI: string;
+      KINDE_REDIRECT_URI: string;
+      KINDE_DOMAIN: string;
 
     };
 }
