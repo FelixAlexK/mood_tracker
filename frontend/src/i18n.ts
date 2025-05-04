70b412819b3f2c699886c4b507f65ab3122f63d8
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-05-04 16:03:23 +0200

    fix: format JSON and TypeScript files for consistency and readability

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,15 +1,16 @@
-import { createI18n } from 'vue-i18n'
-import en from '../locales/en.json'
-import de from '../locales/de.json'
+import { createI18n } from "vue-i18n";
 
-type MessageSchema = typeof en | typeof de
+import de from "../locales/de.json";
+import en from "../locales/en.json";
 
-export default createI18n<[MessageSchema], 'en' | 'de'>({
-    locale: 'en', // set default locale
-    fallbackLocale: 'de', // fallback to German
-    messages: {
-        en: en,
-        de: de
-    },
+type MessageSchema = typeof en | typeof de;
 
-})
+export default createI18n<[MessageSchema], "en" | "de">({
+  locale: "en", // set default locale
+  fallbackLocale: "de", // fallback to German
+  messages: {
+    en,
+    de,
+  },
+
+});
